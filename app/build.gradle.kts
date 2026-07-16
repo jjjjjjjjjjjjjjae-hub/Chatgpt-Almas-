1bf50a38bf44d41b57522208614b2a461a67c2c3
jjjjjjjjjjjjjjae-hub <jjjjjjjjjjjjjjae-hub@example.com>
2026-07-16 18:40:40 +0500

    Part 3: AndroidManifest and Accessibility

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.almas.virtualandroid"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.almas.virtualandroid"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1 Beta"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation(platform("androidx.compose:compose-bom:2025.01.01"))
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+
+    implementation("androidx.navigation:navigation-compose:2.9.0")
+
+    implementation("androidx.appcompat:appcompat:1.7.1")
+
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+}
